fed0bb0f8fde34dac91ac2cccc7ca211ced0e0ea
tshimo73 <tshimologomoeng08@gmail.com>
2025-10-17 13:33:52 +0200

    Finance Calculator

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './views/Home'
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
+  )
+}
